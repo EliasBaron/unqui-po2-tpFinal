414bb6e818686a6999f934bf21627d1faa26e9b6
Florencia06P <fpalavecino743@gmail.com>
2022-10-15 20:00:05 -0300

    Creacion clase Proyecto

---
+++ src/ar/edu/unq/po2/tpFinal/Proyecto.java
@@ -0,0 +1,42 @@
+package ar.edu.unq.po2.tpFinal;
+
+public class Proyecto {
+	private String titulo;
+	private String descripcion;
+	private List<String> categorias;
+	private List<Usuario> usuarios;
+	private List<Muestra> muestras;
+
+	public Proyecto(String titulo, String descripcion, List<String> categorias) {
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.categorias = categorias;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public List<String> getCategorias() {
+		return categorias;
+	}
+
+	public void addUsuario(Usuario usuario) {
+		usuarios.add(usuario);
+
+	}
+
+	public List<Muestra> getMuestras() {
+		return muestras;
+	}
+
+	public void addMuestra(Muestra muestra) {
+		muestras.add(muestra);
+
+	}
+
+}
