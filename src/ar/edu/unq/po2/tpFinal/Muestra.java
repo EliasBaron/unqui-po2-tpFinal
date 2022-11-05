eb7523a62772586af91e8f7d1e4b11c2b0749ed5
EliasBaron <eliasnbaron@gmail.com>
2022-11-05 14:16:40 -0300

    Implementacion test de Muestra con mockito.

--- src/ar/edu/unq/po2/tpFinal/Muestra.java
+++ src/ar/edu/unq/po2/tpFinal/Muestra.java
@@ -5,21 +5,15 @@ import java.util.Date;
 public class Muestra {
 
 	private Date fecha;
-	private double hora;
 	Coordenada coordenada;
 
-	public Muestra(Date fecha, double hora, Coordenada coordenada) {
+	public Muestra(Date fecha, Coordenada coordenada) {
 		this.fecha = fecha;
-		this.hora = hora;
 		this.coordenada = coordenada;
 	}
 
 	public Date getFecha() {
 		return fecha;
-	}
-
-	public double getHora() {
-		return hora;
 	}
 
 	public Coordenada getCoordenada() {
---
+++ test/ar/edu/unq/po2/tpFinal/MuestraTest.java
@@ -0,0 +1,39 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MuestraTest {
+	Muestra muestra; //SUT
+	
+	private Date fechaMuestra; //DOC
+	
+	private Coordenada coordenadaMuestra; //DOC
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		fechaMuestra = mock(Date.class);
+		coordenadaMuestra = mock(Coordenada.class);
+		
+		muestra = new Muestra(fechaMuestra, coordenadaMuestra);
+	
+	}
+
+	@Test
+	void testGetFecha() {
+		assertEquals(fechaMuestra, muestra.getFecha());
+	}
+	
+	@Test
+	void testGetCoordenada() {
+		assertEquals(coordenadaMuestra, muestra.getCoordenada());
+	}
+
+}
