590bd2edd74d375a61bedb0f7a748660188a71cf
EliasBaron <eliasnbaron@gmail.com>
2022-11-05 19:58:02 -0300

    Tests de DesafioAceptado, DesafioNoAceptado y DesafioCompleto.

---
+++ test/ar/edu/unq/po2/tpFinal/TestDesafioCompleto.java
@@ -0,0 +1,57 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestDesafioCompleto {
+	
+	DesafioCompleto desafioCompleto;
+	DesafioUsuario desafioUsuario;
+	Date fechaSuperacion;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		desafioUsuario = mock(DesafioUsuario.class);
+		
+		desafioCompleto = new DesafioCompleto(desafioUsuario);
+	}
+
+	@Test
+	void testGetMomentoSuperacion() {
+		when(desafioUsuario.getMomentoSuperacion()).thenReturn(fechaSuperacion);
+		
+		assertEquals(fechaSuperacion, desafioCompleto.getMomentoSuperacion(fechaSuperacion));
+	}
+	
+	@Test
+	void testGetEstaCompleto() {
+		assertTrue(desafioCompleto.getEstaCompleto());
+	}
+	
+	@Test
+	void testEstaAceptadoAlMomento() {
+		assertFalse(desafioCompleto.estaAceptadoAlMomento());
+	}
+	
+	@Test
+	void testSerAceptadoDesafioUsuario() {
+		desafioCompleto.serAceptadoDesafioUsuario(desafioUsuario); // EL METODO NO HACE NADA
+	}
+	
+	@Test
+	void testEvaluarMuestra() {
+		desafioCompleto.evaluarMuestra(null, null); // EL METODO NO HACE NADA
+	}
+	
+	@Test
+	void puedeSerCalificado() {
+		assertTrue(desafioCompleto.puedeSerCalificado());
+	}
+
+}
--- test/ar/edu/unq/po2/tpFinal/TestDesafioNoAceptado.java
+++ test/ar/edu/unq/po2/tpFinal/TestDesafioNoAceptado.java
@@ -1,19 +1,44 @@
 package ar.edu.unq.po2.tpFinal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestDesafioNoAceptado {
 
+	DesafioNoAceptado desafioNoAceptado; //SUT
+	DesafioUsuario desafioUsuario; //DOC
+	EstadoDesafioUsuario estado;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		desafioUsuario = mock(DesafioUsuario.class);
+		estado = mock(EstadoDesafioUsuario.class);
+		
+		desafioNoAceptado = new DesafioNoAceptado(desafioUsuario);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetMomentoSuperacion() {
+		assertEquals(null, desafioNoAceptado.getMomentoSuperacion(null));
+	}
+	
+	@Test
+	void testGetEstaCompleto() {
+		assertFalse(desafioNoAceptado.getEstaCompleto());
+	}
+	
+	@Test
+	void testEstaAceptadoAlMomento() {
+		assertFalse(desafioNoAceptado.estaAceptadoAlMomento());
+	}
+	
+	@Test
+	void testPuedeSerCalificado() {
+		assertFalse(desafioNoAceptado.puedeSerCalificado());
 	}
 
 }
